ddc210e660c483bd6149dd112c291a97a95d5fc3
tusharsir <kathalkar101@gmail.com>
2024-12-09 21:46:01 +0530

    First Commit

---
+++ All_testcases_all_broswers.bat
@@ -0,0 +1,4 @@
+pytest -v -n auto --html=HtmlReports/my_report_chrome.html --browser chrome
+pytest -v -n auto --html=HtmlReports/my_report_firefox.html --browser firefox
+pytest -v -n auto --html=HtmlReports/my_report_edge.html --browser edge
+pytest -v -n auto --html=HtmlReports/my_report_headless.html --browser headless
---
+++ utilities/additional_utilities.py
@@ -0,0 +1,22 @@
+# take screenshot
+# explicit wait
+# dependency --> title--> dont got to login skip
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class additional_utilities_class:
+    @staticmethod
+    def take_screenshot(driver, testcase_name, status):
+        return driver.save_screenshot(f".\\Screenshots\\{testcase_name}_{status}.png")
+
+
+    @staticmethod
+    def explicit_wait(driver, element, time_for_wait=5):
+        try:
+            WebDriverWait(driver, time_for_wait).until(expected_conditions.visibility_of_element_located(element))
+        except :
+            pass
+
+
